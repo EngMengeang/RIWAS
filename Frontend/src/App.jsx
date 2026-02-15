77027cd89ae1e0f24a282f544b8912eb1b648a03
Mengeang <mengeang7899@gmail.com>
2026-02-15 15:23:57 +0700

    Your message

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import SignUp from "./pages/Signup";
 import Login from "./pages/Login";
+import InputResume from "./pages/InputResume";
 function App() {
   return (
     <BrowserRouter>
